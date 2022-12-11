c491bda5cb33094f3a56bc3d72f20d97de83acb0
Jedidja Kakuva <jedidja96@outlook.com>
2022-12-11 11:22:42 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : A program that prints with a printf function
+ *
+ * Return (0)
+ */
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a part of art,\n");
+return (0);
+
+}
