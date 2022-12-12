2cee32f2f10f52d34b4ec5adf0726c08e98caf22
Jedidja Kakuva <jedidja96@outlook.com>
2022-12-12 06:45:48 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include<stdio.h> 
+
 /**
- * main - Entry point
- *
+ * main - Print a string
  * Return : always 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
